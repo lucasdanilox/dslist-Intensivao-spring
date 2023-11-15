0f2d0fb59ec9a1581e0cbaddb0ec48f1f5ffb519
lucasdanilox <lukastecno@gmail.com>
2023-11-14 21:08:16 -0300

    Domain model

---
+++ src/main/java/com/intensivaojava/dslist/entities/Belonging.java
@@ -0,0 +1,25 @@
+package com.intensivaojava.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging(){
+
+    }
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+       id.setGame(game);
+       id.setList(gameList);
+       this.position = position;
+    }
+}
+

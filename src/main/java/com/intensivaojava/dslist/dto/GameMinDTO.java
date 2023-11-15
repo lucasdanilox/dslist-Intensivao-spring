e665403c35ab3c200a024d569ceac4c83f4a3b45
lucasdanilox <lukastecno@gmail.com>
2023-11-14 22:42:19 -0300

    SQL, projection, get games by list

--- src/main/java/com/intensivaojava/dslist/dto/GameMinDTO.java
+++ src/main/java/com/intensivaojava/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.intensivaojava.dslist.dto;
 
 import com.intensivaojava.dslist.entities.Game;
+import com.intensivaojava.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -21,6 +22,15 @@ public class GameMinDTO {
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
+
 
     public Long getId() {
         return id;
--- src/main/java/com/intensivaojava/dslist/services/GameService.java
+++ src/main/java/com/intensivaojava/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.intensivaojava.dslist.services;
 import com.intensivaojava.dslist.dto.GameDTO;
 import com.intensivaojava.dslist.dto.GameMinDTO;
 import com.intensivaojava.dslist.entities.Game;
+import com.intensivaojava.dslist.projections.GameMinProjection;
 import com.intensivaojava.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
 
     }
